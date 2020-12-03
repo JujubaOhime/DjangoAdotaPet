6d80930db0969eecbaf44a76b2bedaaf1830051c
JujubaOhime <ameliadag96@gmail.com>
2020-12-03 20:39:13 -0300

    trocado o settings

--- AdotaPet/settings.py
+++ AdotaPet/settings.py
@@ -121,18 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static/'),
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
-
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
+
 
 
 LOGIN_URL = '/autenticacao/login/'
